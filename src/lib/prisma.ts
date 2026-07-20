cae11308427ee693b6aa25563040bdd51c72d701
jhonemilmnilo <nilojhonemil1@gmail.com>
2026-07-20 17:15:11 +0800

    i added some bugs

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
+import { parse } from 'pg-connection-string';
 
 if (process.env.NODE_ENV !== 'production') {
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
@@ -12,8 +13,15 @@ const prismaClientSingleton = () => {
     throw new Error("DATABASE_URL environment variable is missing!");
   }
   
+  const poolConfig = parse(connectionString);
+  
   const pool = new Pool({
-    connectionString,
+    ...poolConfig,
+    host: poolConfig.host ?? undefined,
+    port: poolConfig.port ? Number(poolConfig.port) : undefined,
+    database: poolConfig.database ?? undefined,
+    user: poolConfig.user ?? undefined,
+    password: poolConfig.password ?? undefined,
     ssl: {
       rejectUnauthorized: false,
     },
